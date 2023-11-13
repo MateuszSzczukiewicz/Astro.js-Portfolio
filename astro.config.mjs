e14756104e88e3d4ad106fc1b834183efed3db10
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-11-13 19:29:19 +0100

    Pierwszy commit na gałęzi main

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
+
+export default defineConfig({
+  integrations: [svelte(), tailwind()],
+  // output: ‘server’,
+  adapter: vercel(),
+});
---
+++ src/components/Card.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let href, title, body;
+</script>
+
+<li>
+	<a href={href}>
+		<h2>
+			{title}
+			<span>&rarr;</span>
+		</h2>
+		<p>{body}</p>
+	</a>
+</li>
+
