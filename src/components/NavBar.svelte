b0309aef3707d8bf932f0ee0292099b33c898d4e
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-11-14 13:36:50 +0100

    Add homepage

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,11 @@
+<script>
+	import NavItem from '@components/NavItem.svelte'
+</script>
+
+<nav class="fixed flex h-20 w-full items-center justify-end">
+	<ul class="mr-32 flex gap-10">
+		<NavItem url="/" text="O mnie" />
+		<NavItem url="/projects" text="Projekty" />
+		<NavItem url="/contact" text="Kontakt" />
+	</ul>
+</nav>
---
+++ src/components/NavItem.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let url, text
+</script>
+
+<li>
+	<a href={url} class="cursor-pointer text-2xl font-semibold">{text}</a>
+</li>
