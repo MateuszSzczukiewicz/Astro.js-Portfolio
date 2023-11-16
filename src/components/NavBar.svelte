cef52c5d5ad8713e9560abde7abd0f90c1e0f2d4
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-11-16 19:38:53 +0100

    Add Home animations and footer

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -2,8 +2,8 @@
 	import NavItem from '@components/NavItem.svelte'
 </script>
 
-<nav class="fixed z-50 hidden h-20 w-full items-center justify-end sm:flex">
-	<ul class="mr-32 flex gap-10">
+<nav class="fixed z-50 hidden h-20 w-full items-center justify-start sm:flex">
+	<ul class="ml-32 mt-12 flex gap-10">
 		<NavItem url="/" text="O mnie" />
 		<NavItem url="/projects" text="Projekty" />
 		<NavItem url="/contact" text="Kontakt" />
