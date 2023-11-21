4e1dbdd8c8826c909e89adc4f06dfcb63e2c81b1
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-11-21 22:02:03 +0100

    Edit add projects page

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,3 @@ import { vitePreprocess } from '@astrojs/svelte'
 export default {
 	preprocess: vitePreprocess()
 }
-
-const config = {
-	kit: {
-		vite: {
-			optimizeDeps: {
-				include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
-			}
-		}
-	}
-}
