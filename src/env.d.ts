33d280a5d4c0988ee841ef909a6e1931beab1502
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-11-27 20:54:19 +0100

    Edit output

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,24 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-declare module '*.astro' {
-	const Component: any
-	export default Component
+declare module '*.svelte' {
+	import { SvelteComponent } from 'svelte'
+	const component: SvelteComponent
+	export default component
+}
+
+declare module 'astro/client' {
+	interface ComponentConstructorOptions<P = Record<string, any>> {
+		target: Element
+		anchor?: Element | null
+		props?: P
+		hydrate?: boolean
+		intro?: boolean
+	}
+
+	type ClientAttributes = { [key: string]: any }
+
+	interface SvelteComponentCustomProps {
+		client?: ClientAttributes
+	}
 }
